1cecca23e08dc4eab47f5b27bfb7c2ad6dad79eb
keerthanasagar04 <keerthanasagar04@gmail.com>
2026-06-30 22:46:56 +0530

    Add files via upload

---
+++ Personal_Budget_Tracker.py
@@ -0,0 +1,23 @@
+print("Welcome to the Budget Tracker!")
+
+name = input("Enter your name: ")
+
+monthly_income = float(input("What is your monthly income?: "))
+rent_expense = float(input("What is your monthly expense on your rent?: "))
+food_expense = float(input("What is your monthly expense on your food?: "))
+travel_expense = float(input("What is your monthly expense on your travel?: "))
+
+total_expense = rent_expense + food_expense + travel_expense
+print(f"Your monthly expense will be {total_expense}.")
+
+money_remaining = monthly_income - total_expense
+print(f"Your monthly savings will be {money_remaining}.")
+
+savings_percentage = round((money_remaining / monthly_income) * 100, 2)
+print(f"Your monthly savings percentage will be {savings_percentage}%.")
+
+if savings_percentage >= 20:
+    print("You are saving enough money!")
+else:
+    print("You need to save more money!")
+
---
+++ Student_Report_Card_Genarator.py
@@ -0,0 +1,37 @@
+print("Welcome to Report Card Generator!")
+
+students_name = input("Enter student name: ")
+roll_number = input("Enter student roll number: ")
+
+english_marks = float(input("What is your marks in English?: "))
+hindi_marks = float(input("What is your marks in Hindi?: "))
+math_marks = float(input("What is your marks in Math?: "))
+science_marks = float(input("What is your marks in Science?: "))
+social_marks = float(input("What is your marks in Social?: "))
+
+total_marks = english_marks + hindi_marks + math_marks + science_marks + social_marks
+print(f"Total Marks: {total_marks}.")
+
+average = round(total_marks / 5 ,2)
+print(f"Average: {average}.")
+
+percentage = round((total_marks / 500) * 100 ,2)
+print(f"Percentage: {percentage}%.")
+
+if percentage >= 95:
+    print("Grade: A")
+elif percentage >= 85:
+    print("Grade: B")
+elif percentage >= 70:
+    print("Grade: C")
+elif percentage >= 55:
+    print("Grade: D")
+elif percentage >= 45:
+    print("Grade: E")
+else:
+    print("Grade: F")
+
+if percentage >= 45:
+    print("Congratulations! You have passed in the exam! :)")
+else:
+    print("You have failed in the exam :(")
